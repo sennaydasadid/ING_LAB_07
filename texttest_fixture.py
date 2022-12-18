1216929930fbe459c64ebc3a67539eff700c503c
sennaydasadid <srimache@unsa.edu.pe>
2022-12-18 00:38:26 -0500

    version

--- texttest_fixture.py
+++ texttest_fixture.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import gilded_rose
+from typing import ItemsView
 
-
+from gilded_rose import GildedRose, Item
+import gilded_rose 
+ACCION="Backstage passes to a TAFKAL80ETC concert"
 
 if __name__ == "__main__":
     print ("OMGHAI!")
     items = [
-             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
-             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-             Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+             ItemsView(name="+5 Dexterity Vest", sell_in=10, quality=20),
+             ItemsView(name="Aged Brie", sell_in=2, quality=0),
+             ItemsView(name="Elixir of the Mongoose", sell_in=5, quality=7),
+             ItemsView(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+             ItemsView(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+             ItemsView(name=ACCION, sell_in=15, quality=20),
+             ItemsView(name=ACCION, sell_in=10, quality=49),
+             ItemsView(name=ACCION, sell_in=5, quality=49),
+             ItemsView(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
             ]
 
     days = 2
@@ -28,4 +30,4 @@ if __name__ == "__main__":
         for item in items:
             print(item)
         print("")
-        GildedRose(items).update_quality()
+        GildedRoseTest(items).update_quality() 
